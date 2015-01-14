6e8e89f0533efa670b816850997a07c8454a2ddc
tomjose666 <tomjose0000@gmail.com>
2015-01-14 14:38:26 +0530

    player

--- js/my_js.js
+++
@@ -1,7 +0,0 @@
-//Function To Display Popup
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-function div_hide() {
-document.getElementById('abc').style.display = "none";
-}
